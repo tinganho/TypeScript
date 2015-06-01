bf7b11cc4f5586f5366d0ccdbd32180edee52d4e
Tingan Ho <tingan87@gmail.com>
2015-06-01 23:41:48 +0800

    Correct new lines

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -5527,7 +5527,7 @@ module ts {
 
                 return type;
             }
-            
+
             function narrowTypeByTypePredicate(type: Type, expr: CallExpression, assumeTrue: boolean): Type {
                 if (type.flags & TypeFlags.Any || !assumeTrue) {
                     return type;
